25799c38fdd72dd94c1c46334a7ee5dabcb22e10
Jerin George Mathew <jerin.edassery95@gmail.com>
2019-06-22 00:18:48 +0200

    Revert "using adam optimizer with default values"
    This reverts commit 5f331e22ecfdeba7488a07f05e066437e518cd30.

--- deeper_model.py
+++ deeper_model.py
@@ -44,7 +44,7 @@ def build_model(
 
     model.compile(
         loss='binary_crossentropy',
-        optimizer=Adam(),
+        optimizer=Adam(lr=0.01),
         metrics=['accuracy'])
 
     return model
